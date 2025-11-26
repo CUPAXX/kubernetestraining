9f9fe1f9fd9511cbbfc5e1fcd14599d1400d8857
CUPAXX TECH <56408053+CUPAXX@users.noreply.github.com>
2025-11-26 20:14:46 +0700

    Chapter 2 Submission 7

--- todo-apps/index.js
+++ todo-apps/index.js
@@ -1,11 +1,17 @@
 const express = require("express");
+const crypto = require("crypto");
+
 const app = express();
 const port = 3000;
+const startingString = crypto.randomUUID();
+const startingTimestamp = new Date().toISOString();
 
 app.get("/", (req, res) => {
-  res.send("Todo-apps Backend!");
+  const timestamp = new Date().toISOString();
+  const currentString = crypto.randomUUID();
+  res.send(`Responding with ${timestamp}: ${currentString}`);
 });
 
 app.listen(port, () => {
-  console.log(`Server started in port ${port}`);
+  console.log(`Started with ${startingTimestamp}: ${startingString}`);
 });
