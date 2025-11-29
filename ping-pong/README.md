30d40c0c778215cf878cd59f4f88a8fe3e41c868
CUPAXX TECH <56408053+CUPAXX@users.noreply.github.com>
2025-11-29 16:44:16 +0700

    Chapter 2 Submission 9

---
+++ ping-pong/README.md
@@ -0,0 +1,4 @@
+# Ping Pong Apps
+
+Build with `docker build -t cupaxx/ping-pong .`
+Deploy with `kubectl apply -f manifests`
---
+++ ping-pong/index.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const app = express();
+const port = 3000;
+
+let counter = 0;
+
+app.get("/", (req, res) => {
+  res.send("Ping-Pong-apps Backend!");
+});
+
+app.get("/pingpong", (req, res) => {
+  res.send(`pong ${counter}`);
+  counter++;
+});
+
+app.listen(port, () => {
+  console.log(`Server started in port ${port}`);
+});
