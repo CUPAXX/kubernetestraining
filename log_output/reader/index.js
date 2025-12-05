1ea119dd551cf902be63bbfda0e96eaed3411369
CUPAXX TECH <56408053+CUPAXX@users.noreply.github.com>
2025-12-05 09:55:45 +0700

    Chapter 3 Submission 5

--- log_output/reader/index.js
+++ log_output/reader/index.js
@@ -10,7 +10,7 @@ const port = 3000;
 const directory = path.join("/", "src", "app");
 const infoFile = path.join(directory, "config", "information.txt");
 const PING_PONG_SERVICE_URL = "http://ping-pong-svc:2345";
-const MESSAGE = process.env.MESSGAE || "No message set";
+const MESSAGE = process.env.MESSAGE || "No message set";
 const INFORMATION = fs.readFileSync(infoFile, "utf8");
 
 app.get("/", async (req, res) => {
